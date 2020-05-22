ea2b2cc78937390b5d0db8b255dcfafee8f4d505
Nitin Singh <nitin.singh01@ad.infosys.com>
2020-05-22 11:40:45 +0530

    project

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+
+#First we retrieves information about what packages can be installed, including what updates to currently installed packages packages are available, from Internet sources
+sudo apt-get update &&
+#Make sure you have the necessary packages to allow the use of Docker’s repository
+sudo apt install apt-transport-https ca-certificates curl software-properties-common  -y &&
+#Add Docker’s GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -  &&
+#Add the stable Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"  &&
+#Update your package index and install Docker CE
+sudo apt update &&
+curl -O https://download.docker.com/linux/ubuntu/dists/bionic/pool/edge/amd64/containerd.io_1.2.2-3_amd64.deb  &&
+sudo apt install ./containerd.io_1.2.2-3_amd64.deb  &&
+sudo apt install docker-ce  -y  &&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  &&
+sudo chmod +x /usr/local/bin/docker-compose  &&
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#now we are done with installation and we will write a welcome message
+echo "Welcome Master we are all set to begin this journey !!!"
